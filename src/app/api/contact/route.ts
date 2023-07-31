2e63b73aff84647e7d7dfd5c00f69977a09d0022
daseul-jang <jds940823@naver.com>
2023-08-01 04:10:48 +0900

    feat: contact 페이지 메일 보내는 기능

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,32 @@
+import { sendEmail } from '@/service/email';
+import { error } from 'console';
+import * as yup from 'yup';
+
+const bodySchema = yup.object().shape({
+  from: yup.string().email().required(),
+  subject: yup.string().required(),
+  message: yup.string().required(),
+});
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  if (!bodySchema.isValidSync(body)) {
+    return new Response(JSON.stringify({ message: '메일 전송 실패!!' }), {
+      status: 500,
+    });
+  }
+
+  return sendEmail(body)
+    .then(
+      () =>
+        new Response(JSON.stringify({ message: '메일 전송 성공!!' }), {
+          status: 200,
+        })
+    )
+    .catch((error) => {
+      console.error(error);
+      return new Response(JSON.stringify({ message: '메일 전송 실패!!' }), {
+        status: 500,
+      });
+    });
+}
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,6 +2,7 @@
 
 import { useState, FormEvent, ChangeEvent } from 'react';
 import Banner, { BannerData } from './Banner';
+import { sendContactEmail } from '@/service/contact';
 
 type Form = {
   from: string;
@@ -9,12 +10,14 @@ type Form = {
   message: string;
 };
 
+const DEFAULT_DATA = {
+  from: '',
+  subject: '',
+  message: '',
+};
+
 export default function ContactForm() {
-  const [form, setForm] = useState<Form>({
-    from: '',
-    subject: '',
-    message: '',
-  });
+  const [form, setForm] = useState<Form>(DEFAULT_DATA);
   const [banner, setBanner] = useState<BannerData | null>(null);
 
   const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -24,11 +27,22 @@ export default function ContactForm() {
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(form);
-    setBanner({ message: '성공했어!!', state: 'success' });
-    setTimeout(() => {
-      setBanner(null);
-    }, 3000);
+    sendContactEmail(form)
+      .then(() => {
+        setBanner({
+          message: '메일을 성공적으로 보냈어요 😊',
+          state: 'success',
+        });
+        setForm(DEFAULT_DATA);
+      })
+      .catch(() => {
+        setBanner({ message: '메일을 보내지 못했어요 😢', state: 'error' });
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setBanner(null);
+        }, 3000);
+      });
   };
 
   return (
